363a9df341a5eae2818ac9f04a55f61688496612
nadasuhailAyesh12 <missnadaayesh@gmail.com>
2023-03-25 01:21:23 +0300

    handle uncaught exceptions

--- backend/controllers/ProductController.js
+++ backend/controllers/ProductController.js
@@ -1,48 +1,51 @@
-const expressAsyncHandler = require('express-async-handler')
 const productService = require("../services/ProductService");
+const catchAsyncErrors = require("../middlewars/catchAsyncErrors");
 
-const createProduct = expressAsyncHandler(async (req, res) => {
-    const product = await productService.createProduct(req.body)
+const createProduct = catchAsyncErrors(async (req, res) => {
+    const product = await productService.createProduct(req.body);
     res.status(201).json({
         success: true,
-        product
-    })
-})
+        product,
+    });
+});
 
-const getProducts = expressAsyncHandler(async (req, res) => {
+const getProducts = catchAsyncErrors(async (req, res) => {
     const products = await productService.getProducts();
     res.status(201).json({
         success: true,
         count: products.length,
-        products
-    })
-}
-)
+        products,
+    });
+});
 
-const getSingleProduct = expressAsyncHandler(async (req, res) => {
+const getSingleProduct = catchAsyncErrors(async (req, res) => {
     const product = await productService.getSingleProduct(req.params.id);
     res.status(200).json({
         success: true,
-        product
-    })
-}
-)
+        product,
+    });
+});
 
-const updateProduct = expressAsyncHandler(async (req, res) => {
-    const product = await productService.updateProduct(req.params.id, req.body)
+const updateProduct = catchAsyncErrors(async (req, res) => {
+    const product = await productService.updateProduct(req.params.id, req.body);
     res.status(200).json({
         success: true,
-        product
-    })
-})
+        product,
+    });
+});
 
-const deleteProduct =
-    expressAsyncHandler(async (req, res) => {
-        await productService.deleteProduct(req.params.id)
-        res.status(204).json({
-            success: true,
-        })
-    })
+const deleteProduct = catchAsyncErrors(async (req, res) => {
+    await productService.deleteProduct(req.params.id);
+    res.status(204).json({
+        success: true,
+    });
+});
 
-const productController = { createProduct, getProducts, getSingleProduct, updateProduct, deleteProduct }
+const productController = {
+    createProduct,
+    getProducts,
+    getSingleProduct,
+    updateProduct,
+    deleteProduct,
+};
 module.exports = productController;
--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,16 @@ const { port } = require('./config/enviroment');
 const app = require('./app.js');
 const dbConnection = require('./db/connection');
 
+process.on('uncaughtException', err => {
+    console.log(`Error:${err.message}`);
+    console.log('shutting down the server due to uncaught exception')
+    process.exit(1);
+})
+
 dbConnection()
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(` server is listening on port ${port} `);
 })
+
 
