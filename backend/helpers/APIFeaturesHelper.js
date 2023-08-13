0fbf234d3f6f586c5cc1d96747f1c452519a1ab6
nadasuhailAyesh12 <missnadaayesh@gmail.com>
2023-08-13 12:31:57 +0300

    add sort for product api"

--- backend/helpers/APIFeaturesHelper.js
+++ backend/helpers/APIFeaturesHelper.js
@@ -5,33 +5,25 @@ class APIFeatures {
     }
 
     search() {
-        const keyword = this.queryString.keyword ? {
-            name: {
-                $regex: this.queryString.keyword,
-                $options: 'i'
+        const keyword = this.queryString.keyword
+            ? {
+                name: {
+                    $regex: this.queryString.keyword,
+                    $options: "i",
+                },
             }
-        } :
-            {}
+            : {};
 
-        this.query = this.query.find({ ...keyword })
+        this.query = this.query.find({ ...keyword });
         return this;
     }
 
     filter() {
-        /* since the endpoint for search and filter we need to remove keyword limit page
-         from req.query since they are not attributes in our model
-        then to make query filter price in specific range we need to get use of mongoo 
-        aggregation functions we want to get them from query and use regex to replace 
-        whenever they are greater,less,greaterorequal ,lessorequal with dollar sign
-       front of them
-         */
-
-        const queryCopy = { ...this.queryString }
-        const filedsToRemove = ['keyword', 'limit', 'page'];
-        filedsToRemove.map(element => delete queryCopy[element]);
-        let filterQueryString = JSON.stringify(queryCopy)
-        filterQueryString = filterQueryString.replace(/\b(gt|gte|lt|lte)\b/g, match => `$${match}`)
-        this.query = this.query.find(JSON.parse(filterQueryString))
+        const queryCopy = { ...this.queryString };
+        const filedsToRemove = ["keyword", "limit", "page", "sorter"];
+        filedsToRemove.map((element) => delete queryCopy[element]);
+        let filterQueryString = JSON.stringify(queryCopy);
+        this.query = this.query.find(JSON.parse(filterQueryString));
         return this;
     }
 
@@ -42,6 +34,13 @@ class APIFeatures {
         this.query = this.query.limit(resultsPerPage).skip(skip);
         return this;
     }
+    sort() {
+        this.query =
+            this.queryString.sorter == "price"
+                ? this.query.sort({ price: 1 })
+                : this.query.sort({ ratings: -1 });
+        return this;
+    }
 }
 
-module.exports = APIFeatures
+module.exports = APIFeatures;
--- backend/services/ProductService.js
+++ backend/services/ProductService.js
@@ -2,64 +2,81 @@ const fs = require("fs");
 
 const APIFeatures = require("../helpers/APIFeaturesHelper");
 const ErrorHandler = require("../helpers/ErrorHandlerHelper");
-const { UploadPhotoHelper, destroyPhotoHelper } = require("../helpers/UploadPhotoHelper");
-const productRepository = require("../repositories/ProductRepository")
+const {
+    UploadPhotoHelper,
+    destroyPhotoHelper,
+} = require("../helpers/UploadPhotoHelper");
+const productRepository = require("../repositories/ProductRepository");
 
 const createProduct = async (body) => {
     const product = await productRepository.createProduct(body);
     return product;
-}
+};
 
 const getProducts = async (requestQuery) => {
-    const productsCount = await productRepository.getProducts().count
+    const productsCount = await productRepository.getProducts().count;
 
-    const apiFeatures = new APIFeatures(productRepository.getProducts().displayProducts, requestQuery)
+    const apiFeatures = new APIFeatures(
+        productRepository.getProducts().displayProducts,
+        requestQuery
+    )
         .search()
         .filter()
-        .pagination()
+        .sort()
+        .pagination();
 
     const products = await apiFeatures.query;
-    return { products, productsCount }
-}
+    return { products, productsCount };
+};
 
 const getSingleProduct = async (id) => {
     const product = await productRepository.getSingleProduct(id);
     if (!product) {
-        throw new ErrorHandler("product not found", 404)
+        throw new ErrorHandler("product not found", 404);
     }
-    return product
-}
+    return product;
+};
 
 const updateProduct = async (id, options) => {
     await getSingleProduct(id);
-    const product = await productRepository.updateProduct(id, options)
+    const product = await productRepository.updateProduct(id, options);
     return product;
-}
+};
 
 const deleteProduct = async (id) => {
     const product = await getSingleProduct(id);
-    const image_id = product.image.public_id
+    const image_id = product.image.public_id;
     destroyPhotoHelper(image_id);
-    await productRepository.deleteProduct(id)
-}
+    await productRepository.deleteProduct(id);
+};
 
 const uploadPhoto = async (files) => {
     if (files) {
-        const fileTypes = ['image/jpeg', 'image/png', 'image/jpg'];
+        const fileTypes = ["image/jpeg", "image/png", "image/jpg"];
 
         if (!fileTypes.includes(files.image.mimetype)) {
             throw new ErrorHandler("unsupported file format", 400);
         }
 
-        const cloudPhoto = await UploadPhotoHelper(files.image.tempFilePath, "products");
+        const cloudPhoto = await UploadPhotoHelper(
+            files.image.tempFilePath,
+            "products"
+        );
         const image = {
             url: cloudPhoto.secure_url,
-            public_id: cloudPhoto.public_id
-        }
+            public_id: cloudPhoto.public_id,
+        };
         fs.unlinkSync(files.image.tempFilePath);
         return image;
     }
-}
+};
 
-const productService = { createProduct, getProducts, getSingleProduct, updateProduct, deleteProduct, uploadPhoto }
+const productService = {
+    createProduct,
+    getProducts,
+    getSingleProduct,
+    updateProduct,
+    deleteProduct,
+    uploadPhoto,
+};
 module.exports = productService;
